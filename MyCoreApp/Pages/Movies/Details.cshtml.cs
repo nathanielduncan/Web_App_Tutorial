2b111929481170ff437ae6bf1f4422d83e4929db
Nathan <54687384+nathanielduncan@users.noreply.github.com>
2021-12-17 16:33:28 -0600

    Step two finished.
    Adds a scaffold movie data model.

---
+++ MyCoreApp/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyCoreApp.Data;
+using MyCoreApp.Models;
+
+namespace MyCoreApp.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MyCoreApp.Data.MyCoreAppContext _context;
+
+        public DetailsModel(MyCoreApp.Data.MyCoreAppContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ MyCoreApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyCoreApp.Data;
+using MyCoreApp.Models;
+
+namespace MyCoreApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyCoreApp.Data.MyCoreAppContext _context;
+
+        public IndexModel(MyCoreApp.Data.MyCoreAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
